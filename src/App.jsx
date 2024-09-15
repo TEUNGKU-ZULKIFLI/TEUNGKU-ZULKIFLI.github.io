b305ba3b6c6e8ed8bfdfedddbba0b368b8ecd995
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-09-15 21:41:39 +0000

    BAGIAN | 007 📝CHANGE MODEL AND ADDING SCROLLING

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import { Experience } from './components/Experience';
+import { ScrollControls } from '@react-three/drei';
+import Experience from './components/Experience';
+
 
 function App() {
   return (
     <Canvas>
-      <color attach="background" args={['#3a0e4c']} />
+      <ScrollControls pages={4} damping={0.5} >
         <Experience />
+      </ScrollControls>
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,15 @@
+import React from "react";
+import { Room } from "/src/components/Room";
 import { OrbitControls } from "@react-three/drei";
-import { Home } from "./Home";
 
 export function Experience() {
+    
     return (
         <>
-        <OrbitControls enabled={false} />
-        <ambientLight intensity={1} color="#ffffff" />
-        <pointLight intensity={100} color="#8A2BE2" position={[0, -1, 0]} />
-        <directionalLight intensity={1} color="#ffffff" />
-        <Home scale={0.19}/>
+        <OrbitControls />
+        <Room scale={0.3} />
         </>
     );
 }
+
+export default Experience;
